407f8ea62a3f4ee5c787717fa14142acd9ed36ee
jakiro <bmeliksetov@gmail.com>
2023-09-24 20:00:42 +0300

    Калькулятор

--- untitled/src/main/java/hello.java
+++ untitled/src/main/java/hello.java
@@ -1,5 +1,11 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello wold");
+        System.out.println(2+6);
+        System.out.println(7-4);
+        System.out.println(3*6);
+        System.out.println(45/8);
+        System.out.println("privet " + " ya" + " tebya" + " lublu");
+        System.out.println("74-ruf=" + (8 + 9));
     }
 }
